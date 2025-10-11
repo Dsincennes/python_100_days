4a16c5219b1d691ccc7798faa775363a32a4bc25
Donald Sincennes <Donaldsincennes@hotmail.com>
2025-10-11 15:17:48 -0400

    day 1 complete

---
+++ Day 1/task/task.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator.")
+born_city = input("What city did you grow up in?\n")
+name_first_pet = input("what is the name of your first pet\n")
+print("Your band name will be " + born_city + " " + name_first_pet + "!")
