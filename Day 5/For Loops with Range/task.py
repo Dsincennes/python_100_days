ae71a09880d827c5ba8fd282bd5b1cec613425da
Donald Sincennes <Donaldsincennes@hotmail.com>
2025-10-18 12:01:09 -0400

    day 5 complete

---
+++ Day 5/For Loops with Range/task.py
@@ -0,0 +1,13 @@
+for number in range(1,11, 2):
+    print(number)
+
+for number in range(0, 11, 1):
+    print(number)
+
+total = 0
+for number in range(1, 101):
+    total+= number
+
+print(total)
+
+
--- Day 5/Highest Score/task.py
+++ Day 5/Highest Score/task.py
@@ -1,2 +1,28 @@
 student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
-print(range(1, 10))
+
+total_exam_scores = sum(student_scores)
+
+sum = 0
+max = max(student_scores)
+min = min(student_scores)
+new_min = student_scores[0]
+new_max = student_scores[0]
+
+for score in student_scores:
+    sum += score
+
+for mini in student_scores:
+    if new_min > mini:
+        new_min = mini
+    if new_max < mini:
+        new_max = mini
+
+print(f"min = {new_min}")
+print(new_max)
+
+
+print(min)
+print(max)
+print(sum)
+print(total_exam_scores)
+
